3522ce5e0b8995aacb04f17f204dcc326340191c
Essami123 <essamalkhateeb88@gmail.com>
2024-05-12 17:20:35 +0300

    new 5/12

--- js/style-switcher.js
+++ js/style-switcher.js
@@ -8,9 +8,9 @@ styleSwitcherToggle.addEventListener("click", () =>
 window.addEventListener("scroll", () => 
 {
     if(document.querySelector(".style-switcher").classList.contains("open"))
-    {
-        document.querySelector(".style-switcher").classList.remove("close");
-    }
+        {
+            document.querySelector(".style-switcher").classList.remove("open");
+        }
 })
  /* ======================================== theme colors ================================================== */
  const alternateStyles = document.querySelectorAll(".alternate-style");
